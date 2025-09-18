f2db68cbb1aade5af53796fd8f845d4d7b0f2381
Alex Naglich <alexnaglich@gmail.com>
2025-09-18 15:02:53 -0400

    added a vite base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] });
+export default defineConfig({
+  base: "/2025_VIS_CLASS/",
+  plugins: [tailwindcss(), svelte()],
+});
