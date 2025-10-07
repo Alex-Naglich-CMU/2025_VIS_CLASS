47bf7d3f1877973c336ebb6ba8966d85918f0bce
Alex Naglich <alexnaglich@gmail.com>
2025-10-07 02:04:52 -0400

    Finished Finished Part 1

--- src/routes/assignments/a3/+page.svelte
+++ src/routes/assignments/a3/+page.svelte
@@ -1,90 +1,17 @@
 <script lang="ts">
-	import AQIChart from '$lib/a3/AQIChart.svelte';
-	import * as d3 from 'd3';
-
-	const datasets = {
-		'Avalon': 'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BAvalon%5D_daily-avg.csv',
-		'Glassport High Street':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BGlassport%20High%20Street%5D_daily-avg.csv',
-		'Lawrenceville':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLawrenceville%5D_daily-avg.csv',
-		'Liberty (SAHS)':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLiberty%20(SAHS)%5D_daily-avg.csv',
-		'Manchester':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BManchester%5D_daily-avg.csv',
-		'North Braddock':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BNorth%20Braddock%5D_daily-avg.csv',
-		'Parkway East (Near Road)':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BParkway%20East%20(Near%20Road)%5D_daily-avg.csv',
-		'USA-Pennsylvania-Pittsburgh':
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BUSA-Pennsylvania-Pittsburgh%5D_daily-avg.csv'
-	};
-
-	let selectedDataset: keyof typeof datasets = $state('Avalon');
-
-	const data = $derived.by(() =>
-		d3.csv(datasets[selectedDataset], (d: any) => ({
-			city: d.City,
-			country: d.Country,
-			mainPollutant: d['Main pollutant'],
-			pm25: +d['PM2.5'],
-			state: d.State,
-			stationName: d['Station name'],
-			timestamp: new Date(d['Timestamp(UTC)']),
-			usAqi: +d['US AQI']
-		}))
-	);
-
-	let sortedByEntryCount = $derived.by(async () => {
-		// Why do I need this type here. Typescript makes no sense but the copilot said to do it...
-		type DatasetKey = keyof typeof datasets;
-
-		const keys = Object.keys(datasets) as DatasetKey[];
-
-		// I hate promises so much. None of this makes any sense to me at all. What on earth is a then? And I'm making an array of promises? This is fake code. Fake news.
-		const promises = keys.map((key) =>
-			d3.csv(datasets[key]).then((data) => ({ key, length: data.length }))
-		);
-		const resolvedData = await Promise.all(promises);
-
-		const sortedEntries = resolvedData.sort((a, b) => a.length - b.length);
-
-		return sortedEntries;
-	});
+	import { resolve } from '$app/paths';
 </script>
 
-<div class="mb-4 flex items-center justify-center gap-4">
-	<h2>AQI Chart for:</h2>
-
-	<select class="select select-lg select-accent" bind:value={selectedDataset}>
-		{#await sortedByEntryCount}
-			<!-- promise is pending -->
-			<option disabled selected>Loading datasets...</option>
-		{:catch error}
-			<!-- promise was rejected -->
-			<option disabled selected>Error loading datasets: {error.message}</option>
-
-		{:then sortedEntries}
-			{#each sortedEntries as { key, length }}
-				<option value={key}>{key} ({length})</option>
-			{/each}
-		{/await}
-	</select>
+<div class="flex-col space-y-4 text-center">
+	<h1>Hello Adventurer</h1>
+	<h2>You have a decision to make....</h2>
+	<h3>Which part of Assignment 3 would you like to view?</h3>
+	<h4>Choose wisely, for your journey awaits!</h4>
+	<div class="flex justify-center gap-4">
+		<a class="btn btn-primary" href={resolve('/assignments/a3/1')}>Part 1</a>
+		<a class="btn btn-primary text-green-600 bg-gray-600" href={resolve('/assignments/a3/2')}>Part 2</a>
+	</div>
+	<small style="opacity:.1;"
+		>Certainly neither of the options is <i> poisoned</i> or anything...</small
+	>
 </div>
-
-{#await data}
-	<!-- promise is pending -->
-	<p>loading data...</p>
-{:then data}
-	<!-- promise was fulfilled or not a Promise -->
-	<AQIChart {data} />
-{:catch error}
-	<!-- promise was rejected -->
-	<p>Something went wrong: {error.message}</p>
-{/await}
-
-<style>
-	* {
-		font-family: sans-serif;
-	}
-</style>
---
+++ src/routes/assignments/a3/2/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<small>Ahh, that was the poisoned chalice, wasn't it?</small>
+<br />
+<br />
+<span>
+	Perhaps you were meant to choose the other path... check back here later for new adventure!
+</span>
